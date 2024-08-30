2f7433f6768413ad170c8e018fd948bce664a31a
George-Freedland <george.freedland@gmail.com>
2024-08-30 05:19:50 -0700

    add online users and versioning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('ws://localhost:8000');
+// const socket = io('ws://localhost:8000');
+const socket = io('ws://192.168.86.28:8000');
 
 const App = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState('');
   const [color, setColor] = useState('');
+  const [onlineUsers, setOnlineUsers] = useState(0);
 
   useEffect(() => {
     socket.on('chat message', (msg) => {
@@ -18,6 +20,16 @@ const App = () => {
 
     return () => {
       socket.off('chat message');
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on('online users', (count) => {
+      setOnlineUsers(count);
+    });
+
+    return () => {
+      socket.off('online users');
     };
   }, []);
 
@@ -37,9 +49,9 @@ const App = () => {
   }, [messages]);
 
   useEffect(() => {
-    const nameSet1 = ['Boring', 'Amazing', 'Wicked', 'Speedy', 'Happy', 'Sad', 'Hopeful', 'Dreamy', 'Savage', 'Laughing', 'Trippy', 'Lost', 'Funny', 'Flawless'];
-    const nameSet2 = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet', 'Grey', 'Black', 'White'];
-    const nameSet3 = ['Banana', 'Apple', 'Cherry', 'Laptop', 'Rabbit', 'Whale', 'Ocean', 'House', 'Robot', 'Phone', 'Gem', 'Marble', 'Speaker', 'Light', 'Bottle', 'Mouse'];
+    const nameSet1 = ['Boring', 'Amazing', 'Wicked', 'Speedy', 'Happy', 'Sad', 'Hopeful', 'Dreamy', 'Savage', 'Laughing', 'Trippy', 'Lost', 'Funny', 'Flawless', 'Perfect', 'Stable', 'Active', 'Rich'];
+    const nameSet2 = ['Lime', 'Teal', 'Maroon', 'Red', 'Orange', 'Yellow', 'Green', 'Aqua', 'Fuchsia', 'Violet', 'Grey', 'Black', 'White'];
+    const nameSet3 = ['Banana', 'Apple', 'Cherry', 'Laptop', 'Rabbit', 'Whale', 'Ocean', 'House', 'Robot', 'Phone', 'Gem', 'Marble', 'Speaker', 'Light', 'Bottle', 'Mouse', 'Rose', 'Flower', 'Corridor'];
     setUsername(nameSet1[Math.floor(Math.random() * nameSet1.length)] + nameSet2[Math.floor(Math.random() * nameSet2.length)] + nameSet3[Math.floor(Math.random() * nameSet3.length)])
     setColor(nameSet2[Math.floor(Math.random() * nameSet2.length)])
   }, []);
@@ -53,29 +65,34 @@ const App = () => {
   };
 
   return (
-    <div className='container'>
-      <h1>World Chat</h1>
-      <h4>Hi<div style={{ color, marginLeft: '0.25rem' }}>{username}</div>!</h4>
-      <form onSubmit={sendMessage}>
-        <input 
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder='Type a message...'
-          maxLength={200}
-        />
-        <button type='submit'>Send</button>
-      </form>
-      <div className='chat-box'>
-        {messages.map((msg, idx) => (
-          <div 
-            className={`chat-message ${msg.isNew ? 'fade-in-scale' : ''}`} 
-            key={idx}
-          >
-            <div style={{ color: msg.color }}>{msg.username}</div>: {msg.message}
-          </div>
-        ))}
+    <>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div>V0.0.7</div><div>Online Users: {onlineUsers}</div>
       </div>
-    </div>
+      <div className='container'>
+        <h1>World Chat</h1>
+        <h4>Hi<div style={{ color, marginLeft: '0.25rem' }}>{username}</div>!</h4>
+        <form onSubmit={sendMessage}>
+          <input 
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder='Type a message...'
+            maxLength={200}
+          />
+          <button type='submit'>Send</button>
+        </form>
+        <div className='chat-box'>
+          {messages.map((msg, idx) => (
+            <div 
+              className={`chat-message ${msg.isNew ? 'fade-in-scale' : ''}`} 
+              key={idx}
+            >
+              <div style={{ color: msg.color }}>{msg.username}</div>: {msg.message}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
