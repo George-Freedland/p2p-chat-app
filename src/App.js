aa9e306a04a4d0734b688de168c2c443bc3a9725
George-Freedland <george.freedland@gmail.com>
2024-08-30 03:32:12 -0700

    add styling

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ const socket = io('ws://localhost:8000');
 const App = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+  const [color, setColor] = useState('White');
 
   useEffect(() => {
     socket.on('chat message', (msg) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
-      // setMessages([...messages, msg]);
+      setMessages((prevMessages) => [msg, ...prevMessages]);
     });
 
     return () => {
@@ -18,22 +19,26 @@ const App = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const nameSet1 = ['Boring', 'Amazing', 'Wicked', 'Speedy', 'Happy', 'Sad', 'Hopeful', 'Dreamy', 'Savage', 'Laughing', 'Trippy', 'Lost', 'Funny', 'Flawless'];
+    const nameSet2 = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet', 'Grey', 'Black', 'White'];
+    const nameSet3 = ['Banana', 'Apple', 'Cherry', 'Laptop', 'Rabbit', 'Whale', 'Ocean', 'House', 'Robot', 'Phone', 'Gem', 'Marble', 'Speaker', 'Light', 'Bottle', 'Mouse'];
+    setUsername(nameSet1[Math.floor(Math.random() * nameSet1.length)] + nameSet2[Math.floor(Math.random() * nameSet2.length)] + nameSet3[Math.floor(Math.random() * nameSet3.length)])
+    setColor(nameSet2[Math.floor(Math.random() * nameSet2.length)])
+  }, []);
+
   const sendMessage = (e) => {
     e.preventDefault();
     if (message.trim()) {
-      socket.emit('chat message', message);
+      socket.emit('chat message', { message, username, color });
       setMessage('');
     }
   };
 
   return (
-    <div>
-      <h1>World's Best P2P Chat App</h1>
-      <div>
-        {messages.map((msg, idx) => (
-          <div key={idx}>{msg}</div>
-        ))}
-      </div>
+    <div className='container'>
+      <h1>World Chat</h1>
+      <h4>Hi<div style={{ color, marginLeft: '0.25rem' }}>{username}</div>!</h4>
       <form onSubmit={sendMessage}>
         <input 
           value={message}
@@ -42,6 +47,11 @@ const App = () => {
         />
         <button type='submit'>Send</button>
       </form>
+      <div className='chat-box'>
+        {messages.map((msg, idx) => (
+          <div className='chat-message' key={idx}><div style={{ color: msg.color }}>{msg.username}</div>: {msg.message}</div>
+        ))}
+      </div>
     </div>
   );
 }
